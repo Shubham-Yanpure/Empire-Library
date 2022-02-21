6c384d2fa9e65c49bc908d0bf757f562c0dc67f1
Shubham Yanpure <94111650+Shubham-Yanpure@users.noreply.github.com>
2022-02-21 16:44:57 +0530

    Added enter key to login

--- js/login.js
+++ js/login.js
@@ -12,3 +12,18 @@ loginbtn.addEventListener('click',function(){
         window.alert("Incorrect Username or Password");
     }
 });
+
+loginbtn.addEventListener("keyup", function(event){
+    if(event.key=="Enter"){
+       let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if(username=="admin" && password=="admin@123"){
+        location.assign("main.html");
+    }else if(username=="" && password==""){
+        window.alert("Please enter Username/Password")
+    }else{
+        window.alert("Incorrect Username or Password");
+    }
+   }
+})
